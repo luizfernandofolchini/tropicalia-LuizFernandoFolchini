dfe07a7b2e71c68ca00a4a1cc4eb3095da9e2dd3
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-04-01 14:05:19 -0300

    1/04

--- script.js
+++ script.js
@@ -12,5 +12,5 @@ document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 diminuiFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte -=0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    });
+  });
 });
