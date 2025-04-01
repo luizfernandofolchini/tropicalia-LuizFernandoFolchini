4b3e73fe93edc712475e7f4ed219bacd41c7dd10
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-04-01 14:51:47 -0300

    1/04

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-const botaoDeAcessibilidade = document.getElementById('botao-acebilidade');
-const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade');
+    const botaoDeAcessibilidade = document.getElementById('botao-acebilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
-botaoDeAcessibilidade.addEventListener('click', function(){
-botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-});
-
-
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    });
 
 
-const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-let tamanhoAtualFonte = 1;
 
- aumentaFonteBotao.addEventListener('click', function(){
-tamanhoAtualFonte +=0.1;
-document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-});
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-diminuiFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte -=0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-  });
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
 });
