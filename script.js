df716b20fe7f5a43dcb831153ce7b5b01501b900
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-04-01 14:04:28 -0300

    1/04

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let tamanhoAtualFonte = 1;
 
  aumentaFonteBotao.addEventListener('click', function(){
 tamanhoAtualFonte +=0.1;
-document.body.style.fontSize = '${tamanhoAtualFonte}rem';
+document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 });
 
 diminuiFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte -=0.1;
-    document.body.style.fontSize = '${tamanhoAtualFonte}rem';
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
 });
