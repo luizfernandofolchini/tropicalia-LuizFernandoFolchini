b154859e3c04dd413ba8f0cccd27e6a9b17d6ff5
luizfernandofolchini <147443269+luizfernandofolchini@users.noreply.github.com>
2025-04-15 14:41:38 -0300

    15/04

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste');
+
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function () {
@@ -25,4 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
+    })
 });
